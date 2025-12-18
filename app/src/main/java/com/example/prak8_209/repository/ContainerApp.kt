9157353e495a012429a11eb661c3758a4e5e05f6
lululthfh <punyalulu05@gmail.com>
2025-12-18 14:06:35 +0700

    feat: menambahkan interface ContainerApp

---
+++ app/src/main/java/com/example/prak8_209/repository/ContainerApp.kt
@@ -0,0 +1,13 @@
+package com.example.prak8_209.repository
+
+import com.example.prak8_209.service.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
+}
