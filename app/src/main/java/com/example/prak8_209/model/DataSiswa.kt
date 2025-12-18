b1f3e67e21260eafaa25c39f19fea15d9cee8082
lululthfh <punyalulu05@gmail.com>
2025-12-18 10:34:05 +0700

    feat: menambahkan function DetailSiwa yang mengambil data dari DataSiswa

--- app/src/main/java/com/example/prak8_209/model/DataSiswa.kt
+++ app/src/main/java/com/example/prak8_209/model/DataSiswa.kt
@@ -19,3 +19,9 @@ data class DetailSiswa(
     val alamat: String = "",
     val telpon: String = ""
 )
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
