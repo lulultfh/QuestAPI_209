e2d7d0653ff6de52c6022e80da96b421af0f9297
lululthfh <punyalulu05@gmail.com>
2025-12-18 10:28:10 +0700

    chore: setup gradle, dependecies, plugins dan import library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -49,6 +50,21 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    //icons
+    implementation(libs.compose.icons)
+    //navigasi
+    implementation(libs.navigation.compose)
+    // Retrofit
+    implementation(libs.retrofit2.kotlinx.serialization.converter) //retrofit-serialization
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.coil.compose)
+    implementation(libs.kotlinx.serialization.json)
+    //vm
+//    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
