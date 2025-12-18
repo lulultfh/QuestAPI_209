ae581f02e1548ae9f5a38da1761cf45dd8957c27
lululthfh <punyalulu05@gmail.com>
2025-12-18 14:01:38 +0700

    feat: menambahkan class JaringanRepositoryDataSiswa yang mengimplementasikan function getDataSiswa dan postDataSiswa

--- app/src/main/java/com/example/prak8_209/repository/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/prak8_209/repository/RepositoryDataSiswa.kt
@@ -1,8 +1,16 @@
 package com.example.prak8_209.repository
 
 import com.example.prak8_209.model.DataSiswa
+import com.example.prak8_209.service.ServiceApiSiswa
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
 }
