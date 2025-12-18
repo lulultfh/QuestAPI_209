f763c631ac8eb850a3e6cc782cffecbb1f9d349c
lululthfh <punyalulu05@gmail.com>
2025-12-18 17:48:31 +0700

    feat: menambahkan pengimplementasian dari interface DestinasiNavigasi yakni DestinasiHome untuk mendefinisikan route home dan title resource yang mengambil dari String nama_app

---
+++ app/src/main/java/com/example/prak8_209/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.prak8_209.view.route
+
+import com.example.prak8_209.R
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
