602cc679bd47e535bd211cae159f40b19ee333e8
lululthfh <punyalulu05@gmail.com>
2025-12-25 11:34:42 +0700

    feat: menambahkan variabel uiStateSiswa yang berupa mutableStateOf dengan private set (setter) pada class EditViewModel

--- app/src/main/java/com/example/prak8_209/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/prak8_209/viewmodel/EditViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.prak8_209.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.prak8_209.model.UIStateSiswa
 import com.example.prak8_209.repository.RepositoryDataSiswa
 
 class EditViewModel (savedStateHandle: SavedStateHandle,private val repositoryDataSiswa: RepositoryDataSiswa)
-    : ViewModel(){}
+    : ViewModel(){
+        var uiStateSiswa by mutableStateOf(UIStateSiswa())
+                private set
+    }
