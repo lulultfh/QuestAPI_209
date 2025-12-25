0bbd37cf3ae849fc4ec7e81d81ee2a6caada65e2
lululthfh <punyalulu05@gmail.com>
2025-12-25 10:49:29 +0700

    feat: menambahkan function editSatuSiswa dalam interface RepositoryDataSiswa untuk mengupdate data siswa berdasarkan id

--- app/src/main/java/com/example/prak8_209/repository/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/prak8_209/repository/RepositoryDataSiswa.kt
@@ -7,6 +7,7 @@ interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
     suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
 }
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
