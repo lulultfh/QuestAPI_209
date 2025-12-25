e9c03fda95638e56801f44338f0228bcee838015
lululthfh <punyalulu05@gmail.com>
2025-12-25 11:38:58 +0700

    feat: menambahkan inisialisasi EditViewModel untuk memuat data 1 DataSiswa berdasarkan idSiswa dari RepositoryDataSiswa dan memetakannya ke UIStateSiswa pada class EditViewModel

--- app/src/main/java/com/example/prak8_209/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/prak8_209/viewmodel/EditViewModel.kt
@@ -16,4 +16,9 @@ class EditViewModel (savedStateHandle: SavedStateHandle,private val repositoryDa
         var uiStateSiswa by mutableStateOf(UIStateSiswa())
                 private set
     private  val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa).toUIStateSiswa(true)
+        }
+    }
     }
