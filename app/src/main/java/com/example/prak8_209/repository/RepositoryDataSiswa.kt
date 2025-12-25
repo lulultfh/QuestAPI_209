e2faeb17f138abe159870c87a84426b67847fef8
lululthfh <punyalulu05@gmail.com>
2025-12-25 10:54:54 +0700

    feat: mengimplementasikan editSatuSiswa pada class JaringanRepositoryDataSiswa yang mengedit berdasarkan id menggunakan serviceApiSiswa

--- app/src/main/java/com/example/prak8_209/repository/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/prak8_209/repository/RepositoryDataSiswa.kt
@@ -2,6 +2,7 @@ package com.example.prak8_209.repository
 
 import com.example.prak8_209.model.DataSiswa
 import com.example.prak8_209.service.ServiceApiSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
@@ -16,4 +17,6 @@ class JaringanRepositoryDataSiswa(
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
     override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
 }
