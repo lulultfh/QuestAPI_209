0009ca62bda73ab7a6f352f3d4fd7ebd3b58d3b1
lululthfh <punyalulu05@gmail.com>
2025-12-25 11:50:56 +0700

    feat: menambahkan function editSatuSiswa untuk melakukan update DataSIswa ke repositori berdasarkan idSiswa pada class EditViewModel

--- app/src/main/java/com/example/prak8_209/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/prak8_209/viewmodel/EditViewModel.kt
@@ -8,9 +8,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.prak8_209.model.DetailSiswa
 import com.example.prak8_209.model.UIStateSiswa
+import com.example.prak8_209.model.toDataSiswa
 import com.example.prak8_209.model.toUIStateSiswa
 import com.example.prak8_209.repository.RepositoryDataSiswa
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class EditViewModel (savedStateHandle: SavedStateHandle,private val repositoryDataSiswa: RepositoryDataSiswa)
     : ViewModel(){
@@ -30,5 +32,15 @@ class EditViewModel (savedStateHandle: SavedStateHandle,private val repositoryDa
     fun updateUiState(detailSiswa: DetailSiswa){
         uiStateSiswa =
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa())
+            if (call.isSuccessful){
+                println("Update sukses: ${call.message()}")
+            }else{
+                println("Update error: ${call.errorBody()}")
+            }
+        }
     }
     }
