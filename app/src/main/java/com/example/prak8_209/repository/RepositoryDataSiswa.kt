7ce5c7d83836c7e79d5ed602725ee19fda1d372c
lululthfh <punyalulu05@gmail.com>
2025-12-25 10:52:10 +0700

    feat: mengimplementasikan getSatuSiswa pada class JaringanRepositoryDataSiswa yang mengambil berdasarkan id daru serviceApiSiswa

--- app/src/main/java/com/example/prak8_209/repository/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/prak8_209/repository/RepositoryDataSiswa.kt
@@ -15,5 +15,5 @@ class JaringanRepositoryDataSiswa(
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 }
